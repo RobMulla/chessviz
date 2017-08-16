5575661c0743bee7d63398c27b8ea7ca2e3fc640
Rob Mulla <rob.mulla@marriott.com>
2017-08-16 10:47:13 -0400

    Adding master comparison

--- app/js/compareChart.js
+++ app/js/compareChart.js
@@ -1,81 +1,309 @@
+$(document).ready(function() {
 
-var makechart = function() {
+    var processed_json = new Array();   
 
-    $(document).ready(function() {
+    var optionsb = {
+        title: {
+            text: 'Bishop Wierdness'
+        },
 
-        var processed_json = new Array();   
-
-        var options = {
+        chart: {
+            renderTo: 'compare_players_b',
+            type: 'line'
+        },
+        yaxis: {
             title: {
-                text: 'Player Wierdness'
-            },
+                text: 'Deviation'
+            }
+        },
+        series: [{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}],
+        dashStyle: 'longdash',
 
-            chart: {
-                renderTo: 'compare_players',
-                type: 'line'
-            },
-            series: [{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
-            {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
-            {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
-            {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
-            {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
-            {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
-            {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
-            {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
-            {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
-            {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
-            {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
-            {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
-            {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
-            {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
-            {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
-            {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}],
-            dashStyle: 'longdash',
-
-            tooltip: {
-                pointFormat: '{series.name} {series.value}'
-            },
-            legend: {
-                enabled: false
-            },
-            plotOptions: {
-                line: {
-                    marker: {
-                        enabled: false
-                    }
+        tooltip: {
+            pointFormat: '{series.name} {series.value}'
+        },
+        legend: {
+            enabled: false
+        },
+        plotOptions: {
+            line: {
+                marker: {
+                    enabled: false
                 }
             }
-        };
+        }
+    }; 
 
-        // Read in all the comparison data
-        $.getJSON('./data/master_player_comparison.json', function(data) {
+    var optionsp = {
+        title: {
+            text: 'Pawn Wierdness'
+        },
 
-            var piecetype = document.getElementById('heatmap_piece');
-            console.log("JSON",piecetype.options[piecetype.selectedIndex].value)
+        chart: {
+            renderTo: 'compare_players_p',
+            type: 'line'
+        },
+        series: [{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}],
+        dashStyle: 'longdash',
 
-            all_data = data['b']['white_moves_deviation']
-
-            // TRY TO MAKE AN ARRAY CONTAINING THE DATA
-
-            var allplayersArray = []; // This will be the resulting array
-            for(var key in all_data) {
-                var entry = d3.values(all_data[key]); // values
-                entry.name = key; // key?
-                allplayersArray.push(entry)
+        tooltip: {
+            pointFormat: '{series.name} {series.value}'
+        },
+        legend: {
+            enabled: false
+        },
+        plotOptions: {
+            line: {
+                marker: {
+                    enabled: false
+                }
             }
+        }
+    }; 
 
-            console.log("allplayersArray",allplayersArray)
-            console.log("d3.values(allplayersArray)",d3.values(allplayersArray[0]))
 
-            for (i = 0; i < 238; i++){
-                options.series[i].name = allplayersArray[i].name
-                options.series[i].data = allplayersArray[i]
+    var optionsr = {
+        title: {
+            text: 'Rook Wierdness'
+        },
+
+        chart: {
+            renderTo: 'compare_players_r',
+            type: 'line'
+        },
+        series: [{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}],
+        dashStyle: 'longdash',
+
+        tooltip: {
+            pointFormat: '{series.name} {series.value}'
+        },
+        legend: {
+            enabled: false
+        },
+        plotOptions: {
+            line: {
+                marker: {
+                    enabled: false
+                }
             }
-            
-            var chart = new Highcharts.Chart(options);
-        });
+        }
+    }; 
+
+    var optionsq = {
+        title: {
+            text: 'Queen Wierdness'
+        },
+
+        chart: {
+            renderTo: 'compare_players_q',
+            type: 'line'
+        },
+        series: [{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}],
+        dashStyle: 'longdash',
+
+        tooltip: {
+            pointFormat: '{series.name} {series.value}'
+        },
+        legend: {
+            enabled: false
+        },
+        plotOptions: {
+            line: {
+                marker: {
+                    enabled: false
+                }
+            }
+        }
+    };
+
+    var optionsk = {
+        title: {
+            text: 'King Wierdness'
+        },
+
+        chart: {
+            renderTo: 'compare_players_k',
+            type: 'line'
+        },
+        series: [{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}],
+        dashStyle: 'longdash',
+
+        tooltip: {
+            pointFormat: '{series.name} {series.value}'
+        },
+        legend: {
+            enabled: false
+        },
+        plotOptions: {
+            line: {
+                marker: {
+                    enabled: false
+                }
+            }
+        }
+    }; 
+
+    $.getJSON('./data/master_player_comparison.json', function(data) {
+
+        //load data for each piece
+        all_data_b = data['b']['white_moves_deviation']
+        all_data_p = data['p']['white_moves_deviation']
+        //all_data_n = data['n']['white_moves_deviation']
+        all_data_r = data['r']['white_moves_deviation']
+        all_data_q = data['q']['white_moves_deviation']
+        all_data_k = data['k']['white_moves_deviation']
+
+
+        // BISHOP CHART SETUP
+        // MAKE AN ARRAY CONTAINING THE DATA
+        var allplayersArrayb = []; // This will be the resulting array
+        for(var key in all_data_b) {
+            var entry = d3.values(all_data_b[key]); // values
+            entry.name = key; // key?
+            allplayersArrayb.push(entry)
+        }
+
+        for (i = 0; i < 238; i++){
+            optionsb.series[i].name = allplayersArrayb[i].name
+            optionsb.series[i].data = allplayersArrayb[i]
+        }
+        var chart = new Highcharts.Chart(optionsb);
+
+        // PAWN CHART SETUP
+        // MAKE AN ARRAY CONTAINING THE DATA
+        var allplayersArrayp = []; // This will be the resulting array
+        for(var key in all_data_p) {
+            var entry = d3.values(all_data_p[key]); // values
+            entry.name = key; // key?
+            allplayersArrayp.push(entry)
+        }
+
+        for (i = 0; i < 238; i++){
+            optionsp.series[i].name = allplayersArrayp[i].name
+            optionsp.series[i].data = allplayersArrayp[i]
+        }
+        var chart = new Highcharts.Chart(optionsp);
+
+        // ROOK CHART SETUP
+        // MAKE AN ARRAY CONTAINING THE DATA
+        var allplayersArrayr = []; // This will be the resulting array
+        for(var key in all_data_r) {
+            var entry = d3.values(all_data_r[key]); // values
+            entry.name = key; // key?
+            allplayersArrayr.push(entry)
+        }
+
+        for (i = 0; i < 238; i++){
+            optionsr.series[i].name = allplayersArrayr[i].name
+            optionsr.series[i].data = allplayersArrayr[i]
+        }
+        var chart = new Highcharts.Chart(optionsr);
+
+        // KING CHART SETUP
+        // MAKE AN ARRAY CONTAINING THE DATA
+        var allplayersArrayk = []; // This will be the resulting array
+        for(var key in all_data_k) {
+            var entry = d3.values(all_data_k[key]); // values
+            entry.name = key; // key?
+            allplayersArrayk.push(entry)
+        }
+
+        for (i = 0; i < 238; i++){
+            optionsk.series[i].name = allplayersArrayk[i].name
+            optionsk.series[i].data = allplayersArrayk[i]
+        }
+        var chart = new Highcharts.Chart(optionsk);
+
+        // QUEEN CHART SETUP
+        // MAKE AN ARRAY CONTAINING THE DATA
+        var allplayersArrayq = []; // This will be the resulting array
+        for(var key in all_data_q) {
+            var entry = d3.values(all_data_q[key]); // values
+            entry.name = key; // key?
+            allplayersArrayq.push(entry)
+        }
+
+        for (i = 0; i < 238; i++){
+            optionsq.series[i].name = allplayersArrayq[i].name
+            optionsq.series[i].data = allplayersArrayq[i]
+        }
+        var chart = new Highcharts.Chart(optionsq);
 
     });
-}
 
-makechart();
+});
